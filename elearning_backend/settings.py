b206ca03ce900a1a407c437ff35515218c41cac2
ifiokLab <ifiokudoh77@gmail.com>
2024-10-20 10:46:43 +0100

    ddddddd

--- elearning_backend/settings.py
+++ elearning_backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['elearning-backend-2iu9.onrender.com']
+ALLOWED_HOSTS = ["elearning-backend-2iu9.onrender.com",]
 
 
 # Application definition
